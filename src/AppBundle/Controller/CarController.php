1a7b35210569587463757a91b165347763c84c6b
bn-fondative <bn@fondative.com>
2016-09-26 23:25:17 +0100

    Create api services for car resources

---
+++ src/AppBundle/Controller/CarController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Car;
+use AppBundle\Manager\CarManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @RouteResource("Car")
+ */
+class CarController extends Controller
+{
+
+    /**
+     * Get cars
+     *
+     * @ApiDoc(
+     *     section="Cars services",
+     *     description="Get cars",
+     *     statusCodes={
+     *      "200": "OK"
+     *     }
+     * )
+     */
+   public function cgetAction(){
+       /** @var CarManager $carManager */
+       $carManager = $this->get('car_manager');
+
+       $cars = $carManager->loadCars();
+
+       return array('cars' => $cars);
+   }
+
+
+    /**
+     * Get car by id
+     *
+     * @ApiDoc(
+     *     section="Cars services",
+     *     description="Get car by id",
+     *     requirements={
+     *      {"name"="id", "requirement"="\d+", "dataType"="integer", "required"=true, "description"="Car Id"},
+     *     },
+     *     statusCodes={
+     *      "200": "OK",
+     *      "404": "Not Found"
+     *     }
+     * )
+     */
+    public function getAction($id){
+        /** @var CarManager $carManager */
+        $carManager = $this->get('car_manager');
+
+        $car = $carManager->loadCar($id);
+
+
+        if($car === null){
+            return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        }
+        return  array('car' => $car);
+    }
+
+    /**
+     * Create car
+     *
+     * @ApiDoc(
+     *     section="Cars services",
+     *     description="Post car",
+     *    input={
+     *      "class"="AppBundle\Entity\Car"
+     *     },
+     *     statusCodes={
+     *      "201": "Created",
+     *      "400": "Error parameters"
+     *     }
+     * )
+     */
+    public function postAction(Request $request){
+
+        $data = $request->request->all();
+
+        /** @var CarManager $carManager */
+        $carManager = $this->get('car_manager');
+
+        $car = $carManager->createCar($data);
+
+        return  array('car' => $car);
+
+    }
+
+
+}
---
+++ src/AppBundle/Manager/CarManager.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Manager;
+
+use AppBundle\Entity\Car;
+use Doctrine\ORM\EntityManager;
+
+
+class CarManager
+{
+
+    private $em;
+    private $repository;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $this->repository = $em->getRepository('AppBundle:Car');
+    }
+
+    public function loadCars(){
+        return $this->repository->findAll();
+    }
+
+    public function loadCar($id){
+        return $this->repository->find($id);
+    }
+
+    public function createCar($data){
+        $car = new Car();
+        $car
+            ->setName($data['name'])
+            ->setMarque($data['marque'])
+            ->setModel($data['model'])
+            ->setDescription($data['description']);
+
+
+        $this->em->persist($car);
+        $this->em->flush();
+
+        return $car;
+
+    }
+
+}
